5a7601d324a22dcb487fd79cc06bb9146562acee
Amdany <amdanycylvia6@gmail.com>
2024-10-22 22:46:17 +0300

    Add files via upload

---
+++ 1D-arrays[1].C
@@ -0,0 +1,11 @@
+//Arrays in C-1D array
+#include<stdio.h>
+int main () {
+  int i;
+  int age [5] = {5,10,15,20,25};
+   for(i=0;i<5;i++){
+   	printf("age[%d] = %d\n",i,age[i]);
+   }
+    
+    return 0;	
+}
